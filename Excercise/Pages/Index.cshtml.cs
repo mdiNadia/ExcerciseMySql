79964b257d1df62b9393508b2a2710b67e5ee507
nadia mohammadi <nadiamuhammadi080@gmail.com>
2022-05-15 14:00:00 -0700

    change first to firstOrDefault

--- Excercise/Pages/Index.cshtml.cs
+++ Excercise/Pages/Index.cshtml.cs
@@ -46,19 +46,19 @@ namespace Excercise.Pages
         [HttpPost]
         public async Task<IActionResult> OnGetReport(string serial)
         {
-            var data = _excerciseService.GetReportData<GetDeviceInfoDto>(serial);
-            //var data = await _excerciseService.GetQueryList().AsNoTracking().Where(c => c.SerialNumber == serial)
-            //    .GroupBy(c => c.DeviceId)
-            //    .Select(g => new GetDeviceInfoDto()
-            //    {
-            //        MachinId = g.Select(r => r.IdMachine).OrderByDescending(r => r).First(),
-            //        RefId = g.Select(r => r.RefId).OrderByDescending(r => r).First(),
-            //        EventId = g.Select(r => r.IdEvent).OrderBy(r => r).First(),
-            //        DeviceId = g.Select(r => r.DeviceId).OrderBy(r => r).First(),
-            //        FirstDate = g.Min(r => r.CreateDate),
-            //        LastDate = g.Max(r => r.CreateDate)
-            //    })
-            //    .ToListAsync();
+            //var data = _excerciseService.GetReportData<GetDeviceInfoDto>(serial);
+            var data = await _excerciseService.GetQueryList().AsNoTracking().Where(c => c.SerialNumber == serial)
+                .GroupBy(c => c.DeviceId)
+                .Select(g => new GetDeviceInfoDto()
+                {
+                    MachinId = g.Select(r => r.IdMachine).OrderByDescending(r => r).FirstOrDefault(),
+                    RefId = g.Select(r => r.RefId).OrderByDescending(r => r).FirstOrDefault(),
+                    EventId = g.Select(r => r.IdEvent).OrderBy(r => r).FirstOrDefault(),
+                    DeviceId = g.Select(r => r.DeviceId).OrderBy(r => r).FirstOrDefault(),
+                    FirstDate = g.Min(r => r.CreateDate),
+                    LastDate = g.Max(r => r.CreateDate)
+                })
+                .ToListAsync();
             var s = data;
 
             return Partial("_ReportPartial",data);
